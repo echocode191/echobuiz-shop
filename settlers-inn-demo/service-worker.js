2cfd1556d91cda05a7e595f51b4d8b06dd1d99d1
echocode <echoworld191@gmail.com>
2025-06-26 18:04:15 +0300

    Update service-worker.js

--- settlers-inn-demo/service-worker.js
+++ settlers-inn-demo/service-worker.js
@@ -1,27 +1,31 @@
 const CACHE_NAME = 'settlers-v1';
 const urlsToCache = [
   '/',
-  '/index.html',
-  '/menu.html',
-  '/about.html',
-  '/gallery.html',
-  '/contact.html',
-  '/accommodation.html',
-  '/assets/logo.png',
-  '/assets/chapatis.jpg',
-  '/assets/tea.jpg',
-  '/assets/coffee.jpg',
-  // add all key assets here
+  'index.html',
+  'menu.html',
+  'about.html',
+  'contact.html',
+  'gallery.html',
+  'accommodation.html',
+  'assets/logo.png',
+  'assets/tea.jpg',
+  'assets/coffee.jpg',
+  'assets/chapatis.jpg',
+  'assets/ugali-fish.jpg',
+  'assets/settlers-bg.mp4',
+  'manifest.json'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
